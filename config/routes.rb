2189bdbf3a3ef338031fb0646d76ee064e897cc3
marcus <marcus.longoria12@gmail.com>
2021-04-05 15:49:42 -0500

    login/logout completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post] 
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   post '/logout' => 'sessions#destroy'
