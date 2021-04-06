04fbd334e062a58f3c45fe713188abe6340a42d0
marcus <marcus.longoria12@gmail.com>
2021-04-06 09:05:24 -0500

    Need to add CRUD functions

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: :create
+
+  def new
+  end
+  
+  def create
+    user = User.find_by(name: params[:user][:name])
+
+    user = user.try(:authenticate, params[:user][:password])
+  
+    return redirect_to(controller: 'sessions', action: 'new') unless user
+  
+    session[:user_id] = user.id
+  
+    @user = user
+  
+    redirect_to controller: 'welcome', action: 'home'
+  end
+  
+  def destroy
+    session.delete :user_id 
+    redirect_to '/'
+  end
+end
