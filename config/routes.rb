bd0c9ba5b34282323fdb496484632c5acef8f9d1
marcus <marcus.longoria12@gmail.com>
2021-04-13 03:08:43 -0500

    added new author/genre links to forms when error.

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,12 @@ Rails.application.routes.draw do
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   post '/logout' => 'sessions#destroy'
-
   delete '/posts/:id', to: 'posts#destroy', as:'post'
 
   resources :users, only: [:new, :create] 
-  
   resources :posts
   resources :authors
   resources :genres
 
   root 'welcome#home'
 end
-
-# Look up routes and resources modules on learn and fix routes and all has_many/belongs_to relationships
