8a27c00131ab3221dd5a66dd28eb23c2cdd159c7
marcus <marcus.longoria12@gmail.com>
2021-04-14 17:10:07 -0500

    need to add omniauth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,20 +6,15 @@ class SessionsController < ApplicationController
   
   def create
     user = User.find_by(username: params[:user][:username])
-
     user = user.try(:authenticate, params[:user][:password])
-  
     return redirect_to(controller: 'sessions', action: 'new') unless user
-  
     session[:user_id] = user.id
-  
     @user = user
-  
     redirect_to controller: 'welcome', action: 'home'
   end
   
   def destroy
     session.delete :user_id 
-    redirect_to '/'
+    redirect_to root_path
   end
 end
